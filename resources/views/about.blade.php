2b58ad72ba42cf20ed96a965ce6597708b97ef06
Eshrak Rahman <eshrakurrahman@gmail.com>
2026-04-15 20:51:25 +0600

    finished 2 h

--- resources/views/about.blade.php
+++
@@ -1,4 +0,0 @@
-<h1>Hello, welcome to my laravel course </h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam ipsa assumenda commodi facilis praesentium vitae quos
-    dolores quidem quaerat laboriosam! Perspiciatis quia blanditiis cumque doloribus cupiditate voluptatibus assumenda
-    ratione! Qui?</p>
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,7 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/', [HomeController::class, 'index']);
